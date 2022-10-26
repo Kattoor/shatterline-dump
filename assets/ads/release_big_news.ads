80a2d6677bdcc0be7cdee348a80029e9d1fed79c
Kattoor <jcatthoor@inimco.com>
2022-10-26 16:47:27 +0200

    2022-10-26

--- assets/ads/release_big_news.ads
+++ assets/ads/release_big_news.ads
@@ -5,15 +5,9 @@
             "loading_error_image": "default_big_news",
             "external_image": { 
                 "localizations": {
-                    "de-de": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "en-us": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "es-es": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "fr-fr": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "it-it": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "pl-pl": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "pt-pt": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "tr-tr": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "uk-ua": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png"
+                    "en-us": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png",
+                    "uk-ua": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png",
+                    "ja-jp": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png"
                 }
             }
         },
@@ -22,15 +16,9 @@
             "loading_error_image": "default_big_news",
             "external_image": { 
                 "localizations": {
-                    "de-de": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "en-us": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "es-es": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "fr-fr": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "it-it": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "pl-pl": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "pt-pt": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "tr-tr": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png",
-                    "uk-ua": "https://www.shatterline.gg/wp-content/uploads/2022/09/founder_pack_big_news.png"
+                    "en-us": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png",
+                    "uk-ua": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png",
+                    "ja-jp": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png"
                 }
             }
         }
--- assets/ads/test_big_news.ads
+++ assets/ads/test_big_news.ads
@@ -5,17 +5,10 @@
             "loading_error_image": "default_big_news",
             "external_image": { 
                 "localizations": {
-                    "de-de": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "en-us": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "es-es": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "fr-fr": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "it-it": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "pl-pl": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "pt-pt": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "ru-ru": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "tr-tr": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "uk-ua": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png",
-                    "test": "https://catoolwebdav-net-cdn.gcdn.co/catool/4ecc687eca7ca2d7a41fdf74f63055df.png"
+                    "en-us": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png",
+                    "uk-ua": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png",
+                    "ja-jp": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png",
+                    "test": "https://www.shatterline.gg/wp-content/uploads/2022/09/roadmap.png"
                 }
             }
         }
